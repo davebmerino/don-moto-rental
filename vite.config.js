9e8019695f4335885d28abaf7059b08b2e64d435
Daavviidd21 <daavviidd21@gmail.com>
2025-06-13 20:11:06 +0800

    config

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(), tailwindcss("tailwind-scrollbar-hide")],
-  base: "/don-moto-rental",
+  base: "/don-moto-rental/",
 });
