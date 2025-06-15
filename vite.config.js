4a942a5512356506809a761dbd34f71e5e4fc60f
Daavviidd21 <daavviidd21@gmail.com>
2025-06-15 20:34:14 +0800

    changeviteconfig

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import { viteStaticCopy } from "vite-plugin-static-copy";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/don-moto-rental/",
+  // base: "/don-moto-rental/",
   plugins: [
     react(),
     tailwindcss(),
