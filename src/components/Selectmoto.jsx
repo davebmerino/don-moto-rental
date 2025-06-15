453cac1f6fa58ffbd0dec328d903c7599887b0d3
Daavviidd21 <daavviidd21@gmail.com>
2025-06-15 20:41:56 +0800

    images

--- src/components/Selectmoto.jsx
+++ src/components/Selectmoto.jsx
@@ -2,36 +2,41 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
 import Motocard from "./Motocard";
+import clickOrange from "../images/clickorange.jpg";
+import clickBlak from "../images/clickblack.png";
+import clickRed from "../images/clickred.avif";
+import Aerox155 from "../images/aerox.webp";
+import nmaxv2 from "../images/nmax.jpg";
 
 const motorcycles = [
   {
     name: "Click 125 Orange",
-    image: "../images/clickorange.jpg",
+    image: clickOrange,
     description: "2019 model, perfect for city riding and affordable",
     available: true,
   },
   {
     name: "Click 125 Black",
-    image: "../images/clickblack.png",
+    image: clickBlak,
     description: "2022 model, perfect for city riding and affordable",
     available: false,
   },
   {
     name: "Click 125 Red",
-    image: "../images/clickred.avif",
+    image: clickRed,
     description: "2022 model, perfect for city riding and affordable",
     available: true,
   },
   {
     name: "Aerox 155 V2",
-    image: "../images/aerox.webp",
+    image: Aerox155,
     description:
       "2024 model, Standard version, perfect for city riding and for long ride",
     available: true,
   },
   {
     name: "Nmax 155 V2",
-    image: "../images/nmax.jpg",
+    image: nmaxv2,
     description:
       "2024 model, ABS version, perfect for city riding and for long ride",
     available: true,
