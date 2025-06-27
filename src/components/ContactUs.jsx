c1c38536fa8f54bee4159d3b2d1cb24241a46dcb
Daavviidd21 <daavviidd21@gmail.com>
2025-06-27 13:06:08 +0800

    Updatemapbug

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -10,16 +10,15 @@ export default function ContactUs() {
         <div className="w-full md:w-1/2 h-[300px] md:h-auto">
           <iframe
             title="Location Map"
-            // src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3924.737708066173!2d123.95246937503606!3d10.362844889761938!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x33a9980b7df24999%3A0xfab3623ff8114ca9!2sGK%20Eversley!5e0!3m2!1sen!2sph!4v1750997977751!5m2!1sen!2sph"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3924.737708066173!2d123.95246937503606!3d10.362844889761938!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x33a9980b7df24999%3A0xfab3623ff8114ca9!2sGK%20Eversley!5e0!3m2!1sen!2sph!4v1750997977751!5m2!1sen!2sph"
             width="600"
             height="450"
-            style="border:0;"
-            allowfullscreen=""
-            loading="lazy"
-            referrerpolicy="no-referrer-when-downgrade"
-            className="w-full h-full rounded-xl shadow-md border-0"
+            style={{ border: 0 }}
             allowFullScreen=""
-          ></iframe>
+            loading="lazy"
+            referrerPolicy="no-referrer-when-downgrade"
+            className="w-full h-full rounded-xl shadow-md border-0"
+          />
         </div>
 
         {/* Form Section */}
